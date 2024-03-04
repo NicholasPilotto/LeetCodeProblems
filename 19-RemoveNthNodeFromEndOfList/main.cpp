2c211c4571f86bdc245255a6666b8a90ae84b247
NicholasPilotto <nicholaspilotto@gmail.com>
2024-03-04 21:32:34 +0100

    End problem

--- 19-RemoveNthNodeFromEndOfList/main.cpp
+++ 19-RemoveNthNodeFromEndOfList/main.cpp
@@ -10,19 +10,58 @@ struct ListNode {
 
 class Solution {
   private:
-    ListNode* revertList(ListNode* node) {
+    ListNode* removeNode(ListNode* node, int& n) {
       if (!node) {
         return nullptr;
       }
 
-      ListNode* newNode = new ListNode(node->val, node);
+      if (node->next) {
+        node->next = removeNode(node->next, n);
+      }
+
+      --n;
+
+      if (n == 0) {
+        return node->next;
+      }
+
+      return node;
     }
 
   public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+      return removeNode(head, n);
     }
 };
 
+void printList(ListNode* n) {
+  if (!n) {
+    return;
+  }
+
+  std::cout << n->val << '\n';
+
+  printList(n->next);
+}
+
 int main() {
+  ListNode* head = new ListNode(
+    1, new ListNode(
+      2, new ListNode(
+        3, new ListNode(
+          4, new ListNode(
+            5, nullptr
+          )
+        )
+      )
+    )
+  );
+
+  Solution s;
+
+  ListNode* n = s.removeNthFromEnd(head, 3);
+
+  printList(n);
+
   return 0;
 }
