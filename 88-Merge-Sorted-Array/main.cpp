f1f75c195010998d19fb521ac1564bec4b010cb5
NicholasPilotto <nicholaspilotto@gmail.com>
2024-04-08 21:31:16 +0200

    Start resolving problem 88

--- 88-Merge-Sorted-Array/main.cpp
+++ 88-Merge-Sorted-Array/main.cpp
@@ -11,10 +11,11 @@ class Solution {
 
       while (i < m) {
         while (y < n) {
-          if (nums1[i] <= nums2[y]) {
+          if (nums1[i] >= nums2[y]) {
+            nums1.insert(nums1.begin() + i, nums2.begin() + y, nums2.begin() + y);
+            ++y;
+          } else if (nums1[i] < nums2[y]) {
             ++i;
-          } else if (nums1[i] > nums2[y]) {
-            nums1.insert(nums1.begin() + i, nums2.begin() + y, )
           }
         }
       }
@@ -22,6 +23,16 @@ class Solution {
 };
 
 int main() {
+  vector<int> nums1 = { 1,2,3,0,0,0 };
+  vector<int> nums2 = { 2,5,6 };
+
+  Solution::merge(nums1, nums1.size(), nums2, nums2.size());
+
+  for (const int n : nums1) {
+    std::cout << n << ' ';
+  }
+
+  std::cout << '\n';
   return 0;
 }
 
